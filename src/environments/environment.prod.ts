07a58116fd50c8f1993caf8c1deacb73e3640a89
Pulkit Agrawal <pulkit@rapido.bike>
2019-06-23 05:08:59 +0530

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   BASE_URL: 'https://pulkit-ide-api.herokuapp.com/',
   apiUrl: 'https://authorization-api.herokuapp.com',
-  POST_API: 'https://post-api.herokuapp.com'
+  POST_API: 'https://pulkit-post-api.herokuapp.com'
 };
